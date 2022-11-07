c07463bd0fbec797d1ddfb2040799b6692b35ffc
Derrick Black <blackfloret@gmail.com>
2022-11-07 13:39:46 -0800

    Changing to new navigation bar

---
+++ app/src/main/java/com/example/juvenilediabetesmanager/ui/pet/PetFragment.kt
@@ -0,0 +1,42 @@
+package com.example.juvenilediabetesmanager.ui.pet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.juvenilediabetesmanager.databinding.FragmentPetBinding
+
+class PetFragment : Fragment() {
+
+    private var _binding: FragmentPetBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val petViewModel =
+            ViewModelProvider(this).get(PetViewModel::class.java)
+
+        _binding = FragmentPetBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textPet
+        petViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/juvenilediabetesmanager/ui/pet/PetViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.juvenilediabetesmanager.ui.pet
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PetViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is pet Fragment"
+    }
+    val text: LiveData<String> = _text
+}
