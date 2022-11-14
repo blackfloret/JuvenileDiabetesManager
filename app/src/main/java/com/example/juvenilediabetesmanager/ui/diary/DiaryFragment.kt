980c6c16c7b669e147832f81397ce2be61b07828
jamey-albert <albertj6@wwu.edu>
2022-11-14 12:17:16 -0800

    Add recycler adaptoer, xml layout (buggy).

--- app/src/main/java/com/example/juvenilediabetesmanager/ui/diary/DiaryFragment.kt
+++ app/src/main/java/com/example/juvenilediabetesmanager/ui/diary/DiaryFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.juvenilediabetesmanager.DiaryRecyclerAdapter
 import com.example.juvenilediabetesmanager.databinding.FragmentDiaryBinding
 
 class DiaryFragment : Fragment() {
 
     private var _binding: FragmentDiaryBinding? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<DiaryRecyclerAdapter.ViewHolder>? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,10 +32,17 @@ class DiaryFragment : Fragment() {
         _binding = FragmentDiaryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDiary
-        diaryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        layoutManager = LinearLayoutManager(getContext())
+        recyclerView.layoutManager = layoutManager
+
+        adapter = DiaryRecyclerAdapter()
+        recyclerView.adapter = adapter
+
+//        val textView: TextView = binding.
+//        diaryViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
